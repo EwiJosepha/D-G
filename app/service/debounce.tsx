721bd2c2fdc3f2c9fd2169897fc5bef4ab2ff81d
ewi josepha <ewijosepha@gmail.com>
2024-04-01 15:21:48 +0200

    create, view and edit profil

--- app/service/debounce.tsx
+++
@@ -1,8 +0,0 @@
-let timeId: ReturnType<typeof setTimeout>; // Track the timer ID
-
-export const debounceFetch = () => {
-  return (fn: () => void, debounce_time = 1000) => {
-    clearTimeout(timeId);
-    timeId = setTimeout(fn, debounce_time);
-  };
-};
