9d655a72199a00a6fab830420df94e9271e6de77
nsamedaisy <nsamedaisy@gmail.com>
2024-04-03 16:56:20 +0100

    dashboard updates

---
+++ app/_components/molecules/animate.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AnimatePage = () => {
+    return (
+        <div>
+            <h1 className="font-bold text-4xl text-gray-500 py-10 animate-walk font-mono"> Complete Your Transaction with D&J Collections and Delight in the Perks</h1>
+        </div>
+    );
+};
+
+export default AnimatePage; 
