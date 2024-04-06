fb4b01b32b0f13d86c605a0f0ff6bd7509e9139a
nsamedaisy <nsamedaisy@gmail.com>
2024-04-06 20:12:10 +0100

    few changes

--- app/(dashboard-details-page)/details/page.tsx
+++ app/(dashboard-details-page)/details/page.tsx
@@ -1,24 +1,24 @@
-// 'use client'
+'use client'
 
-// import ContactPage from "@/components/organisms/contactAgent";
-// import Footer from "@/components/organisms/footer";
-// import DetailNav from "@/components/organisms/detailNav";
-// import DetailHero from "@/components/organisms/detailHero";
-// import DescriptionPage from "@/components/organisms/description";
-// import AnimatePage from "@/app/_components/molecules/animate";
+import ContactPage from "@/components/organisms/contactAgent";
+import Footer from "@/components/organisms/footer";
+import DetailNav from "@/components/organisms/detailNav";
+import DetailHero from "@/components/organisms/detailHero";
+import DescriptionPage from "@/components/organisms/description";
+import AnimatePage from "@/app/_components/molecules/animate";
 
-// const DetailPage: React.FC = () => {
+const DetailPage: React.FC = () => {
 
-//     return (
-//         <div>
-//             <DetailNav />
-//             <DetailHero />
-//             <DescriptionPage />
-//             <ContactPage />
-//             <AnimatePage />
-//             <Footer />
-//         </div>
-//     );
-// };
+    return (
+        <div>
+            <DetailNav />
+            <DetailHero />
+            <DescriptionPage />
+            <ContactPage />
+            <AnimatePage />
+            <Footer />
+        </div>
+    );
+};
 
-// export default DetailPage;
+export default DetailPage;
--- app/_components/organisms/description.tsx
+++ app/_components/organisms/description.tsx
@@ -1,68 +1,68 @@
-// 'use client'
-// import { useRouter } from "next/router";
+'use client'
+import { useRouter } from "next/router";
 
-// import { useAppContext } from "@/store/app-context";
+import { useAppContext } from "@/store/app-context";
 
-// interface DescriptionPageProps {
-//   //
-// }
+interface DescriptionPageProps {
+  //
+}
 
-// const DescriptionPage: React.FC<DescriptionPageProps> = ({ }: DescriptionPageProps) => {
+const DescriptionPage: React.FC<DescriptionPageProps> = ({ }: DescriptionPageProps) => {
 
-//   const { propertyInfo } = useAppContext();
+  const { propertyInfo } = useAppContext();
 
-//   return (
-//     <div className="container mx-auto font-serif py-20">
-//       <div className="items-center justify-center flex flex-col">
-//         <h1 className="text-4xl my-4 ">{propertyInfo.name}</h1>
-//         <p className="text-3xl font-thin">Location </p>
-//       </div>
+  return (
+    <div className="container mx-auto font-serif py-20">
+      <div className="items-center justify-center flex flex-col">
+        <h1 className="text-4xl my-4 ">{propertyInfo.name}</h1>
+        <p className="text-3xl font-thin">Location </p>
+      </div>
 
-//       <div className="md:flex mt-16">
-//         <div className="w-full md:flex justify-between items-center md:w-1/3 p-4 mr-10">
-//           <div className="bg-black text-white rounded shadow-md p-8">
-//             <h2 className="text-2xl font-bold mb-4 font-mono"> Details</h2>
-//             <div className="flex flex-wrap">
-//               <div className="w-full border-t border-gray-600 flex items-center sm:w-1/2 p-4">
-//                 <p className="mr-4">Number of Rooms:</p>
-//                 <p className="text-lg font-semibold"></p>
-//               </div>
-//               <div className="w-full border-t border-gray-600 flex items-center sm:w-1/2 p-4">
-//                 <p className="mr-4">Number of Baths:</p>
-//                 <p className="text-lg font-semibold"></p>
-//               </div>
-//               <div className="w-full border-t border-gray-600 flex items-center sm:w-1/2 p-4">
-//                 <p className="mr-4">Living Room:</p>
-//                 <p className="text-lg font-semibold"></p>
-//               </div>
-//               <div className="w-full border-t border-gray-600 flex items-center sm:w-1/2 p-4">
-//                 <p className="mr-4">Status:</p>
-//                 <p className="text-lg font-semibold"></p>
-//               </div>
-//               <div className="w-full border-t border-gray-600 flex items-center sm:w-1/2 p-4">
-//                 <p className="mr-4">Area:</p>
-//                 <p className="text-lg font-semibold"></p>
-//               </div>
-//               <div className="w-full border-t border-gray-600 flex items-center sm:w-1/2 p-4">
-//                 <p className="mr-4">Price:</p>
-//                 <p className="text-lg font-semibold">$ </p>
-//               </div>
-//             </div>
-//             <div className="mt-4 w-full flex justify-between items-center border-t border-gray-700 py-4">
-//               <p className=" mr-4">Description:</p>
-//               <p></p>
-//             </div>
-//           </div>
-//         </div>
+      <div className="md:flex mt-16">
+        <div className="w-full md:flex justify-between items-center md:w-1/3 p-4 mr-10">
+          <div className="bg-black text-white rounded shadow-md p-8">
+            <h2 className="text-2xl font-bold mb-4 font-mono"> Details</h2>
+            <div className="flex flex-wrap">
+              <div className="w-full border-t border-gray-600 flex items-center sm:w-1/2 p-4">
+                <p className="mr-4">Number of Rooms:</p>
+                <p className="text-lg font-semibold"></p>
+              </div>
+              <div className="w-full border-t border-gray-600 flex items-center sm:w-1/2 p-4">
+                <p className="mr-4">Number of Baths:</p>
+                <p className="text-lg font-semibold"></p>
+              </div>
+              <div className="w-full border-t border-gray-600 flex items-center sm:w-1/2 p-4">
+                <p className="mr-4">Living Room:</p>
+                <p className="text-lg font-semibold"></p>
+              </div>
+              <div className="w-full border-t border-gray-600 flex items-center sm:w-1/2 p-4">
+                <p className="mr-4">Status:</p>
+                <p className="text-lg font-semibold"></p>
+              </div>
+              <div className="w-full border-t border-gray-600 flex items-center sm:w-1/2 p-4">
+                <p className="mr-4">Area:</p>
+                <p className="text-lg font-semibold"></p>
+              </div>
+              <div className="w-full border-t border-gray-600 flex items-center sm:w-1/2 p-4">
+                <p className="mr-4">Price:</p>
+                <p className="text-lg font-semibold">$ </p>
+              </div>
+            </div>
+            <div className="mt-4 w-full flex justify-between items-center border-t border-gray-700 py-4">
+              <p className=" mr-4">Description:</p>
+              <p></p>
+            </div>
+          </div>
+        </div>
 
-//         <div className="w-full items-center md:w-3/5 p-4 text-gray-700 text-base">
-//           <p className="mb-20">Congratulations! You've found a beautiful property with great features. This property offers a healthy and comfortable living environment. Here are some key highlights:  Spacious rooms with plenty of natural light Modern and well-equipped kitchen Beautifully landscaped garden State-of-the-art fitness center Serene and peaceful neighborhood Proximity to parks and green spaces Easy access to hiking and biking trails Healthy and sustainable building materials Energy-efficient appliances Water-saving fixtures Don't miss the opportunity to make this property your own and enjoy a healthy lifestyle!</p>
-//           <p className="mb-6">Contact Josepha and Daisy for more information and/or to schedule a showing of the property you interested in 🥰</p>
-//           <button className="border-2 border-yellow-600 text-yellow-800 py-3 hover:bg-yellow-600 hover:text-white cursor-pointer px-8"> Share Property </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+        <div className="w-full items-center md:w-3/5 p-4 text-gray-700 text-base">
+          <p className="mb-20">Congratulations! You've found a beautiful property with great features. This property offers a healthy and comfortable living environment. Here are some key highlights:  Spacious rooms with plenty of natural light Modern and well-equipped kitchen Beautifully landscaped garden State-of-the-art fitness center Serene and peaceful neighborhood Proximity to parks and green spaces Easy access to hiking and biking trails Healthy and sustainable building materials Energy-efficient appliances Water-saving fixtures Don't miss the opportunity to make this property your own and enjoy a healthy lifestyle!</p>
+          <p className="mb-6">Contact Josepha and Daisy for more information and/or to schedule a showing of the property you interested in 🥰</p>
+          <button className="border-2 border-yellow-600 text-yellow-800 py-3 hover:bg-yellow-600 hover:text-white cursor-pointer px-8"> Share Property </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default DescriptionPage;
+export default DescriptionPage;
